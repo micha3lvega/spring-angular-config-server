df452ab2ba66efbd8a8821616282246165fcbef3
Michael Vega <micha3lvega@gmail.com>
2025-08-11 21:12:49 -0500

    Mostrar las propiedades

--- angular-config-client/src/app/app.component.ts
+++ angular-config-client/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { ConfigService } from './services/config.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-config-client';
+export class AppComponent implements OnInit {
+
+  title = '';
+
+  constructor(private configService: ConfigService) {}
+
+  ngOnInit() {
+    this.configService.getProperty('title').subscribe(value => {
+      console.log('value = ', value);
+      this.title = value;
+    });
+  }
 }
